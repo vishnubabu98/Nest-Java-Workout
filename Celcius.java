9dd9784710586dd68649b7ef7cca0ba9358ddc83
Vishnu Babu <vishnukulakkada444@gmail.com>
2022-10-24 23:29:25 +0530

    Celcius

--- Celcius.java
+++ Celcius.java
@@ -1,1 +1,15 @@
+package Nest-Java-Workout;
+import java.util.Scanner;
 
+class Celcius
+{
+    public static void main (String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        float  cel,fh;
+        System.out.println("Enter an input for converting Fahrenheit to celcius");
+        fh = input.nextFloat();
+        cel  = ((fh-32)*5)/9;
+        System.out.println("Temperature in celsius is: "+cel);
+    }
+}
